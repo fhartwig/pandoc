69c219dac15269148f2b8c2e673f2c8af223db9e
John MacFarlane <jgm@berkeley.edu>
2013-04-10 23:47:48 -0500

    Custom:  further improvements.

--- test.lua
+++ test.lua
@@ -13,6 +13,17 @@ local function escape(s)
     end)
 end
 
+local function attributes(attr)
+  local attr_table = {}
+  for x,y in pairs(attr) do
+    if y and y ~= "" then
+      table.insert(attr_table, ' ' .. x .. '="' .. escape(y) .. '"')
+    end
+  end
+  return table.concat(attr_table)
+end
+
+
 function Str(s)
   return escape(s)
 end
@@ -41,6 +52,14 @@ function Superscript(s)
   return "<sup>" .. s .. "</sup>"
 end
 
+function SmallCaps(s)
+  return '<span style="font-variant: small-caps;">' .. s .. '</span>'
+end
+
+function Strikeout(s)
+  return '<del>' .. s .. '</del>'
+end
+
 function Link(s, src, tit)
   return "<a href='" .. escape(src) .. "' title='" .. escape(tit) .. "'>" .. s .. "</a>"
 end
@@ -49,8 +68,16 @@ function Image(s, src, tit)
   return "<img src='" .. escape(src) .. "' title='" .. escape(tit) .. "'/>"
 end
 
-function Code(s)
-  return "<code>" .. escape(s) .. "</code>"
+function Code(s, attr)
+  return "<code" .. attributes(attr) .. ">" .. escape(s) .. "</code>"
+end
+
+function InlineMath(s)
+  return "\\(" .. escape(s) .. "\\)"
+end
+
+function DisplayMath(s)
+  return "\\[" .. escape(s) .. "\\]"
 end
 
 function Plain(s)
@@ -66,11 +93,7 @@ function Blocksep()
 end
 
 function Header(lev, s, attr)
-  local ident = ""
-  if attr['id'] then
-    ident = " id='" .. escape(attr['id']) .. "'"
-  end
-  return "<h" .. lev .. ident .. ">" .. s .. "</h" .. lev .. ">"
+  return "<h" .. lev .. attributes(attr) ..  ">" .. s .. "</h" .. lev .. ">"
 end
 
 function BlockQuote(s)
@@ -81,8 +104,8 @@ function HorizontalRule()
   return "<hr/>"
 end
 
-function CodeBlock(s)
-  return "<pre><code>" .. escape(s) .. "</code></pre>"
+function CodeBlock(s, attr)
+  return "<pre><code" .. attributes(attr) .. ">" .. escape(s) .. "</code></pre>"
 end
 
 function BulletList(items)
@@ -108,6 +131,14 @@ function DefinitionList(items)
   end
   return "<dl>\n" .. table.concat(buffer, "\n") .. "\n</dl>"
 end
+
+-- function Table(caption, aligns, widths, headers, rows)
+--   local capt = "<caption>" .. caption .. "</caption>\n"
+--   local cols = ""
+--   local head = ""
+--   local body = ""
+--   return "<table>\n" .. capt ..  cols .. head .. body .. "</table>"
+-- end
 
 local meta = {}
 meta.__index =
